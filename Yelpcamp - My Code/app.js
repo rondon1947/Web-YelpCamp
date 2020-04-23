a8c2ff8368a1a1b5d1099533a63058f366a4f795
rondon1947 <rohan.nishant1999@gmail.com>
2020-04-24 04:00:37 +0530

    First commit of Basic structure

---
+++ Yelpcamp - My Code/app.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var campgrounds = [{
+    name: "Salmon Creek",
+    image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
+},
+{
+    name: "Granite Hill",
+    image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
+},
+{
+    name: "Mountain Goat's Rest",
+    image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"
+},
+{
+    name: "Salmon Creek",
+    image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
+},
+{
+    name: "Granite Hill",
+    image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
+},
+{
+    name: "Mountain Goat's Rest",
+    image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"
+},
+{
+    name: "Salmon Creek",
+    image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
+},
+{
+    name: "Granite Hill",
+    image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
+},
+{
+    name: "Mountain Goat's Rest",
+    image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"
+}
+];
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    res.render("landing");
+});
+
+app.get("/campgrounds", function (req, res) {
+    res.render("campgrounds", {campgroundsData: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
+});
+
+app.listen(3000, function () {
+    console.log("The YelpCamp Server has started at localhost:3000");
+});
